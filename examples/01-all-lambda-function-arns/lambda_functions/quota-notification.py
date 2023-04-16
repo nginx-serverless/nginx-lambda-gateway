e5e53ac81dbf863d42c2632e273ed5e72a74489f
Shawn Kim <shawnhan.kim@gmail.com>
2023-04-16 05:35:50 -0700

    feat: quota notification for lambda function ARN and URL

--- examples/01-all-lambda-function-arns/lambda_functions/quota-notification.py
+++ examples/01-all-lambda-function-arns/lambda_functions/quota-notification.py
@@ -1,3 +1,4 @@
+import base64
 import boto3
 import json
 import logging
@@ -14,13 +15,21 @@ def lambda_handler(event, context):
     logger.info("request: " + json.dumps(event))
 
     #topic_arn = os.environ.get('TOPIC_ARN')
-    topic_arn = "arn:aws:sns:us-east-2:{account-id}:nginx-noti"
+    topic_arn = "arn:aws:sns:{region}:{accont-id}:nginx-noti"
     sns_client = boto3.client("sns")
+
+    # Generate a message from the event
+    message = json.dumps(event)
+    if 'body' in event:
+        code = event['body']
+        code_bytes = code.encode('ascii')
+        decoded = base64.b64decode(code_bytes)
+        message = decoded.decode('UTF-8')
 
     try:
         sent_message = sns_client.publish(
             TargetArn=topic_arn,
-            Message=json.dumps(event)
+            Message=message
         )
 
         if sent_message is not None:
@@ -28,7 +37,7 @@ def lambda_handler(event, context):
         return {
             "statusCode": 200,
             "body": {
-                'request': event,
+                'request': json.loads(message),
                 "response": "sent a message to AWS SNS"
             }
         }
